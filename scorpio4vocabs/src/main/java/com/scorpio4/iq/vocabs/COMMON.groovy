fbd32131982776a3b6ecc6c2d20b063fff68cf2b
Lee Curtis <lee.curtis@me.com>
2014-07-10 15:22:28 +1000

    refactored Active Vocabularies. Initial Hydra Vocabulary. added RFC6570 support

---
+++ scorpio4vocabs/src/main/java/com/scorpio4/iq/vocabs/COMMON.groovy
@@ -0,0 +1,14 @@
+package com.scorpio4.iq.vocabs
+
+/**
+ * scorpio4-oss (c) 2014
+ * Module: com.scorpio4.iq.vocabs
+ * User  : lee
+ * Date  : 9/07/2014
+ * Time  : 9:05 PM
+ *
+ *
+ */
+public interface COMMON {
+
+}
